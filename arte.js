d05568dcf9afbe0607d706c4ff4bc270fe7a4dbc
Edouard Leurent <eleurent@gmail.com>
2016-10-04 20:08:48 +0200

    Add iframe support

--- arte.js
+++ arte.js
@@ -30,3 +30,20 @@ $("[arte_vp_url_oembed]").each(function() {
 	    return false;
 	}
 });
+$("iframe").each(function() {
+	src = $(this).attr("src");
+	if (~src.indexOf("json_url")) {
+		url = unescape(src.split('json_url=')[1]);
+	    $.getJSON(url, function(data) {
+			html = '<div class="container"><ul class="span12 functions unstyled">';
+			$.each(data.videoJsonPlayer.VSR, function(i, v) {
+		        if (v.mediaType.indexOf("mp4") > -1) {
+		            html+='<li class="span2 module-grey"><a class="btn btn-block" href="'+v.url+'" target="_blank" download>'+v.versionLibelle + ': ' + v.quality + '</a></li>';
+		        }
+			});
+			html += '</ul></div>';
+			$('body').prepend(html);
+	    });
+	    return false;
+	}
+});
