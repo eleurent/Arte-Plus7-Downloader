a7494985d541e5b1ae40cee4c9cad1370b7b4f6a
Edouard Leurent <eleurent@gmail.com>
2017-04-25 20:17:16 +0200

    Update to Arte new stylesheet
    Keep only iframe support, drop others

--- arte.js
+++ arte.js
@@ -1,48 +1,16 @@
-$("[arte_vp_url]:not([arte_vp_url*=EXTRAIT])").each(function() {
-	$.getJSON($(this).attr("arte_vp_url"), function(data) {
-		html = '<div class="container"><ul class="span12 functions unstyled">';
-		$.each(data.videoJsonPlayer.VSR, function(i, v) {
-	        if (v.mediaType.indexOf("mp4") > -1) {
-	            html+='<li class="span2 module-grey"><a class="btn btn-block" href="'+v.url+'" target="_blank" download>'+v.versionLibelle + ': ' + v.quality + '</a></li>';
-	        }
-		});
-		html += '</ul></div>';
-		$('body').prepend(html);
-	});
-	return false;
-});
-$("[arte_vp_url_oembed]").each(function() {
-	url = $(this).attr("arte_vp_url_oembed");
-	if (url.indexOf("EXTRAIT") < 0) {
-	    $.getJSON(url, function(data) {
-			url = unescape(data.html.split('json_url=')[1]);
-			$.getJSON(url, function(data) {
-				html = '<div class="container"><ul class="span12 functions unstyled">';
-				$.each(data.videoJsonPlayer.VSR, function(i, v) {
-			        if (v.mediaType.indexOf("mp4") > -1) {
-			            html+='<li class="span2 module-grey"><a class="btn btn-block" href="'+v.url+'" target="_blank" download>'+v.versionLibelle + ': ' + v.quality + '</a></li>';
-			        }
-				});
-				html += '</ul></div>';
-				$('body').prepend(html);
-			});
-	    });
-	    return false;
-	}
-});
 $("iframe").each(function() {
 	src = $(this).attr("src");
 	if (~src.indexOf("json_url")) {
 		url = unescape(src.split('json_url=')[1]);
 	    $.getJSON(url, function(data) {
-			html = '<div class="container"><ul class="span12 functions unstyled">';
+			html = '<div class="program-tags"><ul>';
 			$.each(data.videoJsonPlayer.VSR, function(i, v) {
 		        if (v.mediaType.indexOf("mp4") > -1) {
-		            html+='<li class="span2 module-grey"><a class="btn btn-block" href="'+v.url+'" target="_blank" download>'+v.versionLibelle + ': ' + v.quality + '</a></li>';
+		            html+='<li><a href="'+v.url+'" target="_blank" download>'+v.versionLibelle + ': ' + v.quality + '</a></li>';
 		        }
 			});
 			html += '</ul></div>';
-			$('body').prepend(html);
+			$('.program-detail').prepend(html);
 	    });
 	    return false;
 	}
